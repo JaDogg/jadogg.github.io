e9c711f95f3cec79c45d3e35c1992054da2a0a4b
Bhathiya Perera <JaDogg@users.noreply.github.com>
2022-01-19 21:13:04 +0000

    Extract Css to a file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Personal website 
 
 View here -> https://jadogg.github.io/
+
+require `css-minify` and `html-minifier` in path.
--- build.py
+++ build.py
@@ -1,12 +1,16 @@
 import os.path
+import subprocess
+
 import docbox
 
 
 def main():
+    subprocess.run("css-minify -d ./css/ -o ./docs/assets", shell=True)
     docbox.conv(["-r", "--no-number", "-o", "docs/index.html"])
     docbox.conv(["--all-headers-in-toc", "--input", "yaksha_docs", "-o", "docs/yaksha.html"])
     # DocBox documentation
-    docbox.DocBoxApp().convert_text(["--all-headers-in-toc", "-o", "docs/docbox.html"], docbox.__doc__)
+    docbox.DocBoxApp().convert_text(["--all-headers-in-toc", "-o",
+                                     "docs/docbox.html"], docbox.__doc__)
 
 
 if __name__ == "__main__":
