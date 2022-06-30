dbdda0a1482d3ccd34979de98b3bdf475732aa14
Bhathiya Perera <JaDogg@users.noreply.github.com>
2022-06-30 23:06:35 +0100

    system design toolkit added

--- build.py
+++ build.py
@@ -9,6 +9,7 @@ def main():
     subprocess.run("css-minify -d ./css/ -o ./docs/assets", shell=True)
     docbox.conv(["-r", "--no-number", "-o", "docs/index.html", "--posts=posts"], root=path)
     docbox.conv(["--all-headers-in-toc", "--input", "yaksha_docs", "-o", "docs/yaksha.html"], root=path)
+    docbox.conv(["--all-headers-in-toc", "--input", "system_design", "-o", "docs/sdt.html"], root=path)
     # DocBox documentation
     docbox.DocBoxApp(root=path).convert_text(["--all-headers-in-toc", "-o",
                                               "docs/docbox.html"], docbox.__doc__)
